37b50e244cbfb4d6bb5d4769cc7eab298f9b7363
A.Lepe <dev@alepe.com>
2017-10-28 14:56:36 +0900

    2.2.7.1 : fixed JDBCConnector.

---
+++ modules/db/src/test/groovy/jp/sharelock/db/JDBCConnectorTest.groovy
@@ -0,0 +1,17 @@
+package jp.sharelock.db
+
+import spock.lang.Specification
+
+/**
+ * @since 10/28/17.
+ */
+class JDBCConnectorTest extends Specification {
+    def "Testing URL parse"() {
+        setup:
+            def jdbc = new JDBCConnector("","mysql://user1:pass1@localhost1:1234/mydb")
+        expect:
+            def url = jdbc.getJDBCStr()
+            assert url
+            println url
+    }
+}
