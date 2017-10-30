c0d6e2fd6a94ae998d2bef0fa86d836e4c04b008
A.Lepe <dev@alepe.com>
2017-10-30 17:57:55 +0900

    2.2.10.5 : Added Cache expiration and Singleton CacheObj

---
+++ modules/db/src/main/groovy/jp/sharelock/db/CacheObj.groovy
@@ -0,0 +1,15 @@
+package jp.sharelock.db
+
+import groovy.transform.CompileStatic
+
+/**
+ * This is an implementation of Cache
+ * as singleton. So it can be used as:
+ *
+ * CacheObj.instance.get("X")
+ *
+ * @since 17/10/30.
+ */
+@CompileStatic
+@Singleton
+class CacheObj extends Cache<Object> {}
