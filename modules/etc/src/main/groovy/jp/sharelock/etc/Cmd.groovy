38b21c634ea1f6c58cce80b83b673002ad66fd08
A. Lepe <dev@alepe.com>
2017-12-04 15:35:30 +0900

    2.2.24.2 : Renamed execAsync for just async

--- modules/etc/src/main/groovy/jp/sharelock/etc/Cmd.groovy
+++ modules/etc/src/main/groovy/jp/sharelock/etc/Cmd.groovy
@@ -19,15 +19,15 @@ class Cmd {
         new Command().exec(commands, done, fail)
     }
     static void async(final String cmd, final Command.Done done, final Command.Fail fail = null) {
-        new Command().execAsync(cmd, done, fail)
+        new Command().async(cmd, done, fail)
     }
     static void async(final String cmd, List args = [], final Command.Done done = null, final Command.Fail fail = null) {
-        new Command().execAsync(cmd, args, done, fail)
+        new Command().async(cmd, args, done, fail)
     }
     static void async(final List<String> commands, final Command.Done done = null, final Command.Fail fail = null) {
-        new Command().execAsync(commands, done, fail)
+        new Command().async(commands, done, fail)
     }
     static void async(final Map<String,List> commands, final Command.Done done = null, final Command.Fail fail = null) {
-        new Command().execAsync(commands, done, fail)
+        new Command().async(commands, done, fail)
     }
 }
