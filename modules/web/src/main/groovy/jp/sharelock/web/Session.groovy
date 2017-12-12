5329ec8c20c68f0b35bc7ffcd7c205310a412b88
A.Lepe <dev@alepe.com>
2017-12-12 12:21:06 +0900

    2.2.29 : Updated groovy-extends to 1.6.4

--- modules/web/src/main/groovy/jp/sharelock/web/Session.groovy
+++ modules/web/src/main/groovy/jp/sharelock/web/Session.groovy
@@ -1,12 +1,14 @@
 package jp.sharelock.web
 
+import org.eclipse.jetty.websocket.api.Session as JettySession
+import spark.Session as SparkSession
 /**
  * @since 17/04/21.
  */
 @groovy.transform.CompileStatic
 class Session {
-    org.eclipse.jetty.websocket.api.Session websocketSession
-    spark.Session httpSession
+    JettySession websocketSession
+    SparkSession httpSession
     InetAddress address
     String userID
     Map data
