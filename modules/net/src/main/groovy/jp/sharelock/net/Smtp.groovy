dc36cbc2373e7ef3ff8a012127805429029d5239
A.Lepe <dev@alepe.com>
2017-12-13 16:53:17 +0900

    2.2.33 : modified JSON class

--- modules/net/src/main/groovy/jp/sharelock/net/Smtp.groovy
+++ modules/net/src/main/groovy/jp/sharelock/net/Smtp.groovy
@@ -18,7 +18,6 @@ import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeBodyPart
 import javax.mail.internet.MimeMessage
 import javax.mail.internet.MimeMultipart
-import javax.mail.internet.MimePartDataSource
 
 /**
  * References:
