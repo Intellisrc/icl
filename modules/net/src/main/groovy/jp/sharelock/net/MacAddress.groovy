43353477a39f90b32d565bba573188ce989959bc
A.Lepe <dev@alepe.com>
2017-12-15 12:42:06 +0900

    2.2.34.5 : Fixed SysService Error reporting

--- modules/net/src/main/groovy/jp/sharelock/net/MacAddress.groovy
+++ modules/net/src/main/groovy/jp/sharelock/net/MacAddress.groovy
@@ -1,11 +1,13 @@
 package jp.sharelock.net
 
+import groovy.transform.CompileStatic
 import groovy.transform.Immutable
 
 /**
  * @since 17/03/10.
  */
 @Immutable
+@CompileStatic
 class MacAddress {
     byte[] mac
 
@@ -16,20 +18,5 @@ class MacAddress {
     @Override
     String toString() {
         String.format("%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
-    }
-    boolean isVM() {
-        byte[][] virtualMacs = [
-            [0x00, 0x05, 0x69],             //VMWare
-            [0x00, 0x1C, 0x14],             //VMWare
-            [0x00, 0x0C, 0x29],             //VMWare
-            [0x00, 0x50, 0x56],             //VMWare
-            [0x08, 0x00, 0x27],             //Virtualbox
-            [0x0A, 0x00, 0x27],             //Virtualbox
-            [0x00, 0x03, 0xFF],             //Virtual-PC
-            [0x00, 0x15, 0x5D]              //Hyper-V
-        ]
-        return virtualMacs.find {
-            it[0] == mac[0] && it[1] == mac[1] && it[2] == mac[2]
-        }
     }
 }
