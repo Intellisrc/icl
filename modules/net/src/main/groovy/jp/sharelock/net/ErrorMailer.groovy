ac5e881ed7620cd957c459d3cf4cd0dec7b421b0
A. Lepe <dev@alepe.com>
2017-12-27 11:06:31 +0900

    2.2.35.5 : Added Log 'onLog' and created ErrorMailer to send messages on error. Implemented pretty output in JSON

---
+++ modules/net/src/main/groovy/jp/sharelock/net/ErrorMailer.groovy
@@ -0,0 +1,46 @@
+package jp.sharelock.net
+
+import jp.sharelock.etc.Log
+import jp.sharelock.etc.Log.Level
+import jp.sharelock.etc.Version
+import groovy.transform.CompileStatic
+
+/**
+ * Send error and security alerts to administrator
+ * @since 17/12/27.
+ */
+@CompileStatic
+class ErrorMailer {
+    static Level minLevel = Level.ERROR
+    /**
+     * Override the next strings in case
+     * it is displaying incorrect information
+     */
+    static String sysVersion = ""
+    static String hostName = ""
+    static String ipAddress = ""
+    /**
+     * Set ErrorMailer
+     * @param destiny
+     */
+    static void set(Email destiny = null) {
+        Log.onLog = {
+            Level level, String message, Log.Stack stack ->
+                if(level >= minLevel) {
+                    def body  = "Date:" + new Date().toYMDHms() +
+                                "Message: $message" +
+                                "At: " + stack.fileName + ":" + stack.lineNumber
+                                "Class: " + stack.className +
+                                "Method: " + stack.methodName +
+                                "Host:" + hostName ?: InetAddress.getLocalHost().getHostName() +
+                                "IP:" + ipAddress ?: InetAddress.getLocalHost().getHostAddress() +
+                                "Version:" + sysVersion ?: Version.get()
+                    def mailer = new Smtp()
+                    if(!destiny) {
+                        destiny = mailer.from ? new Email(mailer.from) : new Email("root@localhost")
+                    }
+                    mailer.send(destiny.toString(), "[" + level.name() + "] $message", body)
+                }
+        }
+    }
+}
