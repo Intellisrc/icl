e57e7e4da02a506718cd3a981d45f61c659382a6
A. Lepe <dev@alepe.com>
2018-03-21 10:51:22 +0900

    2.2.45.4 : A missign return class and typos

---
+++ modules/net/src/main/groovy/jp/sharelock/net/UDPServer.groovy
@@ -0,0 +1,37 @@
+package jp.sharelock.net
+
+import jp.sharelock.etc.Log
+
+/**
+ * @since 12/29/17.
+ */
+class UDPServer {
+    interface ServerCallback {
+        String exec(String clientCommand)
+    }
+    /**
+     * Set this flag to true to exit the server
+     */
+    private boolean exit = false
+    /**
+     * Constructor and launch server
+     * @param port
+     * @param callback
+     */
+    UDPServer(int port, ServerCallback callback) {
+        DatagramSocket serverSocket = new DatagramSocket(port)
+        byte[] receiveData = new byte[1024]
+        while(exit) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length)
+            serverSocket.receive(receivePacket)
+            String sentence = new String( receivePacket.data )
+            Log.v("RECEIVED: " + sentence)
+            InetAddress IPAddress = receivePacket.address
+            int remotePort = receivePacket.port
+            String capitalizedSentence = sentence.toUpperCase()
+            byte[] sendData = capitalizedSentence.bytes
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, remotePort)
+            serverSocket.send(sendPacket)
+        }
+    }
+}
