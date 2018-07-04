7710c2eab92db26018c92d51ee4be163e0cf42da
A. Lepe <dev@alepe.com>
2018-07-04 11:59:50 +0900

    2.4.5 Added PGP encryption

---
+++ modules/crypt/src/test/groovy/com/intellisrc/crypt/PGPTest.groovy
@@ -0,0 +1,41 @@
+package com.intellisrc.crypt
+
+import com.intellisrc.crypt.encode.PGP
+import com.intellisrc.etc.Bytes
+import org.bouncycastle.openpgp.PGPEncryptedDataGenerator
+import spock.lang.Specification
+
+
+/**
+ * PGP Tests
+ * @since 18/07/04.
+ */
+class PGPTest extends Specification {
+    def "General test using random key"() {
+        setup:
+            PGP pgp = new PGP()
+            byte[] original = "This is a super secret message. Please don't read it out loud!".bytes
+        expect:
+            byte[] encrypted = pgp.encrypt(original)
+            println "encrypted data = '" + Bytes.toHex(encrypted) + "'"
+            byte[] decrypted= pgp.decrypt(encrypted)
+            println "decrypted data = '"+Bytes.toString(decrypted)+"'"
+            assert decrypted == original
+            println "Key: " + Bytes.toString(pgp.key)
+    }
+    def "General test using own key"() {
+        setup:
+        PGP pgp = new PGP(
+                key : "this key is lame".bytes,
+                algorithm: PGPEncryptedDataGenerator.IDEA
+        )
+        byte[] original = "This is a super secret message. Please don't read it out loud!".bytes
+        expect:
+        byte[] encrypted = pgp.encrypt(original)
+        println "encrypted data = '" + Bytes.toHex(encrypted) + "'"
+        byte[] decrypted= pgp.decrypt(encrypted)
+        println "decrypted data = '"+Bytes.toString(decrypted)+"'"
+        assert decrypted == original
+        println "Key: " + Bytes.toString(pgp.key)
+    }
+}
