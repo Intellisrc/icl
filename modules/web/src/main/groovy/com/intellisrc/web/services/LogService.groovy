ef19885e2af3ceaa8d9e832913a349410184ea6f
A. Lepe <dev@alepe.com>
2018-07-12 17:32:51 +0900

    2.4.6 : Added tests for Zip logs. Some code was further split inside Log. Added Log.enable

--- modules/web/src/main/groovy/com/intellisrc/web/services/LogService.groovy
+++ modules/web/src/main/groovy/com/intellisrc/web/services/LogService.groovy
@@ -31,41 +31,38 @@ class LogService implements ServiciableSingle {
                     def type = request.queryParams("type") ?: ""
                     def keyword = request.queryParams("key") ?: ""
                     def res = [:]
-                    if(Log.logFile) {
-                        def log = new File(Log.logFile)
-                        if(log.exists()) {
-                            def logs = log.text.split("\n")
-                            def filtered = logs.findAll {
-                                String line ->
-                                    def parts = line.split("\t")
-                                    LocalDateTime lTime = parts.first().toDateTime()
-                                    def inc = true
-                                    if(from) {
-                                        LocalDateTime dFrom = from.toDateTime()
-                                        inc = lTime >= dFrom
-                                    }
-                                    if(inc && to) {
-                                        LocalDateTime dTo = to.toDateTime()
-                                        inc = lTime <= dTo
-                                    }
-                                    if(inc && keyword) {
-                                        inc = line.contains(keyword)
-                                    }
-                                    if(inc && type) {
-                                        inc = line.contains(" [" + type.toUpperCase() + "] ")
-                                    }
-                                    return inc
-                            }
-                            res = filtered.collect {
-                                String line ->
-                                    def parts = line.split("\t")
-                                    return [
-                                        time : parts.first(),
-                                        type : parts[1].replace("[","").replace("]","").trim(),
-                                        clss : parts[2].trim(),
-                                        mssg : parts[3].trim()
-                                    ]
-                            }
+                    if(Log.logFile.exists()) {
+                        def logs = Log.logFile.text.split("\n")
+                        def filtered = logs.findAll {
+                            String line ->
+                                def parts = line.split("\t")
+                                LocalDateTime lTime = parts.first().toDateTime()
+                                def inc = true
+                                if(from) {
+                                    LocalDateTime dFrom = from.toDateTime()
+                                    inc = lTime >= dFrom
+                                }
+                                if(inc && to) {
+                                    LocalDateTime dTo = to.toDateTime()
+                                    inc = lTime <= dTo
+                                }
+                                if(inc && keyword) {
+                                    inc = line.contains(keyword)
+                                }
+                                if(inc && type) {
+                                    inc = line.contains(" [" + type.toUpperCase() + "] ")
+                                }
+                                return inc
+                        }
+                        res = filtered.collect {
+                            String line ->
+                                def parts = line.split("\t")
+                                return [
+                                    time : parts.first(),
+                                    type : parts[1].replace("[","").replace("]","").trim(),
+                                    clss : parts[2].trim(),
+                                    mssg : parts[3].trim()
+                                ]
                         }
                     }
                     return res
