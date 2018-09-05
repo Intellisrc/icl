00ced7e0a3922da1c4ac38734bba6c98059bea19
A. Lepe <dev@alepe.com>
2018-09-05 17:07:09 +0900

    2.4.9 : Added JarResource and logs should be printed when SNAPSHOT is used.

---
+++ modules/etc/src/main/groovy/com/intellisrc/etc/JarResource.groovy
@@ -0,0 +1,39 @@
+package com.intellisrc.etc
+
+import groovy.transform.CompileStatic
+import java.nio.file.Files
+
+/**
+ * @since 18/09/05.
+ */
+@CompileStatic
+class JarResource {
+    /**
+     * Return a string from a resource inside a Jar
+     * @param path
+     * @return
+     */
+    static String getAsString(Class reference, String path) {
+        InputStream input = reference.getResourceAsStream(path)
+        return new BufferedReader(new InputStreamReader(input)).text
+    }
+    /**
+     * Return a binary file from a Jar
+     * @param path
+     * @return
+     */
+    static byte[] getAsBytes(Class reference, String path) {
+        return reference.getResourceAsStream(path).bytes
+    }
+    /**
+     * Return a file after being copied into a temporally location
+     * @param path
+     * @return
+     */
+    static File getAsTempFile(Class reference, String path) {
+        InputStream input = reference.getResourceAsStream(path)
+        File tempFile = File.createTempFile("jar","tmp")
+        Files.copy(input, tempFile.toPath())
+        return tempFile
+    }
+}
