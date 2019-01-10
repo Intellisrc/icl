8d1fc21ec1db237c1134939b6149433d8491defd
A. Lepe <dev@alepe.com>
2019-01-10 18:34:40 +0900

    2.4.13 : Config.CfgFile added to allow any property file

--- modules/core/src/main/groovy/com/intellisrc/core/Config.groovy
+++ modules/core/src/main/groovy/com/intellisrc/core/Config.groovy
@@ -8,168 +8,161 @@ package com.intellisrc.core
  * @author Alberto Lepe <lepe@intellisrc.com>
  */
 class Config {
-	static String fileName = "config.properties"
-    static String filePath = SysInfo.getUserDir()
-    static Properties props = System.getProperties()
-    private static Properties config = new Properties()
-    private static File configFile
-    private static boolean reload = false
+    static private CfgFile global = new CfgFile(new File(SysInfo.userDir, "config.properties"))
+    static CfgFile system = new CfgFile(null, System.properties)
 
     /**
-     * Be sure filePath ends with File Separator
-     * @param fp
+     * Static methods will call global instance
+     * @param key
+     * @return
      */
-    static void setFilePath(String fp) {
-        if(!fp.endsWith(File.separator)) {
-            fp += File.separator
-        }
-        filePath = fp
-        reload = true
-    }
+    static boolean exists()                     { global.exists() }
+    static boolean matchKey(String key)         { global.matchKey(key) }
+    static boolean hasKey(String key)           { global.hasKey(key) }
+    static String get(String key)               { global.get(key) }
+    static int getInt(String key)               { global.getInt(key) }
+    static double getDbl(String key)            { global.getDbl(key) }
+    static boolean getBool(String key)          { global.getBool(key) }
+    static void set(String key, Object value)   { global.set(key, value) }
 
     /**
-     * Change file name
-     * @param fn
+     * CfgFile can be used with any file
      */
-    static void setFileName(String fn) {
-        fileName = fn
-        reload = true
-    }
+    static class CfgFile {
+        private final Properties config
+        private final File configFile
+        private boolean useFile = false
 
-    static update() {
-        if(!configFile || reload) {
-            configFile = new File(filePath + fileName)
+        CfgFile(File cfgFile = null, Properties props = null) {
+            if(cfgFile) {
+                configFile = cfgFile
+                useFile = true
+            }
+            config = props ?: new Properties()
         }
-        if(configFile.exists()) {
-            if(configFile.canRead()) {
-                config.load(configFile.newDataInputStream())
-                config.keys().each {
-                    String key ->
-                        props.setProperty(key, config.getProperty(key))
+
+        void update() {
+            if(useFile) {
+                if (configFile.exists()) {
+                    if (configFile.canRead()) {
+                        config.load(configFile.newDataInputStream())
+                    }
+                    if (!configFile.canWrite()) {
+                        Log.w("Configuration configFile: " + configFile.toString() + " is not writable. Any attempt to change settings will fail.")
+                    }
+                } else {
+                    Log.v("Configuration not found (" + configFile.toString() + "). All settings will be loaded from System")
                 }
             }
-            if(!configFile.canWrite()) {
-                Log.w( "Configuration configFile: "+configFile.toString()+" is not writable. Any attempt to change settings will fail.")
-            }
-        } else {
-            Log.v( "Configuration not found ("+configFile.toString()+"). All settings will be loaded from System")
         }
-    }
 
-    /**
-     * Returns true if config file exists
-     * @return
-     */
-    static boolean exists() {
-        update()
-        return configFile.exists()
-    }
-
-    /**
-     * Returns true if any key starts with some string
-     * @param key
-     * @return
-     */
-    static boolean matchKey(String key) {
-        update()
-        props.keys().find {
-            String pkey ->
-                pkey.startsWith(key)
+        /**
+         * Returns true if config file exists
+         * @return
+         */
+        boolean exists() {
+            return useFile && configFile.exists()
         }
-    }
 
-    /**
-     *
-     * @param key
-     * @return
-     */
-    static boolean hasKey(String key) {
-        update()
-        return props.containsKey(key)
-    }
-
-    /**
-     * Prevent updating object
-     */
-    static void setProps() {
-        //Do nothing.
-        Log.w( "Properties can not be updated directly. Use set() instead.")
-    }
-
-	/**
-	 * Get value as String
-	 * @param key
-	 * @return
-	 */
-	static String get(String key) {
-        update()
-        String val = ""
-        if(hasKey(key)) {
-            val = config.getProperty(key)
-            if (val == null) {
-                val = ""
-            }
-            if (val.startsWith('"') || val.startsWith("'")) {
-                Log.w("Settings in properties files don't need quotes. Please delete them to remove this warning.")
-                val = val.replaceAll(/^["']|["']$/, '')
+        /**
+         * Returns true if any key starts with some string
+         * @param key
+         * @return
+         */
+        boolean matchKey(String key) {
+            update()
+            config.keys().find {
+                String pkey ->
+                    pkey.startsWith(key)
             }
         }
-        return val
-	}
 
-    /**
-     * Get value as int
-     * @param key
-     * @return
-     */
-    static int getInt(String key) {
-        String str = get(key)
-        if(str.isEmpty()) {
-            str = "0"
+        /**
+         *
+         * @param key
+         * @return
+         */
+        boolean hasKey(String key) {
+            update()
+            return config.containsKey(key)
         }
-        return Integer.parseInt(str)
+
+        /**
+         * Get value as String
+         * @param key
+         * @return
+         */
+        String get(String key) {
+            update()
+            String val = ""
+            if (hasKey(key)) {
+                val = config.getProperty(key)
+                if (val == null) {
+                    val = ""
+                }
+                if (val.startsWith('"') || val.startsWith("'")) {
+                    Log.w("Settings in properties files don't need quotes. Please delete them to remove this warning.")
+                    val = val.replaceAll(/^["']|["']$/, '')
+                }
+            }
+            return val
+        }
+
+        /**
+         * Get value as int
+         * @param key
+         * @return
+         */
+        int getInt(String key) {
+            String str = get(key)
+            if (str.isEmpty()) {
+                str = "0"
+            }
+            return Integer.parseInt(str)
+        }
+
+        /**
+         * Get value as double
+         * @param key
+         * @return
+         */
+        double getDbl(String key) {
+            String str = get(key)
+            if (str.isEmpty()) {
+                str = "0.0"
+            }
+            return Double.parseDouble(str)
+        }
+
+        /**
+         * Get value as boolean
+         * @param key
+         * @return
+         */
+        boolean getBool(String key) {
+            boolean val = true
+            String str = get(key)
+            if (str == null || str.isEmpty() || str == "0" || str.toLowerCase() == "false") {
+                val = false
+            }
+            return val
+        }
+
+        /**
+         * Set value in properties
+         * @param key
+         * @param value
+         */
+        void set(String key, Object value) {
+            update()
+            config.setProperty(key, value.toString())
+            if(useFile) {
+                if (configFile.parentFile.canWrite()) {
+                    config.store(configFile.newWriter(), null)
+                } else {
+                    Log.e("Unable to write to: " + configFile.toString())
+                }
+            }
+        }
     }
-
-    /**
-     * Get value as double
-     * @param key
-     * @return
-     */
-    static double getDbl(String key) {
-        String str = get(key)
-        if(str.isEmpty()) {
-            str = "0.0"
-        }
-        return Double.parseDouble(str)
-    }
-
-    /**
-     * Get value as boolean
-     * @param key
-     * @return
-     */
-    static boolean getBool(String key) {
-        boolean val = true
-        String str = get(key)
-        if(str == null || str.isEmpty() || str == "0" || str.toLowerCase() == "false") {
-            val = false
-        }
-        return val
-    }
-
-	/**
-	 * Set value in properties
-	 * @param key
-	 * @param value 
-	 */
-	static void set(String key, Object value) {
-        update()
-        config.setProperty(key, value.toString())
-        if(configFile.canWrite()) {
-            config.store(configFile.newWriter(), null)
-        } else {
-            Log.e( "Unable to write to: "+configFile.toString())
-        }
-	}
-
 }
