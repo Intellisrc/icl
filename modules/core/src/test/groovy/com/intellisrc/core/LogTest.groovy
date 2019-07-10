222870a97edb472ca99ecda510825e3bdfff19e2
A.Lepe <dev@alepe.com>
2019-07-10 17:47:14 +0900

    2.6.6: Improved Log, Fixed Version, Fixed SCrypt and other improvements

--- modules/core/src/test/groovy/com/intellisrc/core/LogTest.groovy
+++ modules/core/src/test/groovy/com/intellisrc/core/LogTest.groovy
@@ -4,12 +4,21 @@ import spock.lang.Specification
 
 /**
  * @since 17/10/23.
+ *
+ * NOTE: compressing logs is tested in 'etc'
  */
 class LogTest extends Specification {
+    /**
+     * Check if link to last log is created successfully
+     * The link should be created during initialization and even before printing any log (so the log file may not exists yet)
+     * After printing the first message, it will be linked and then created.
+     * @return
+     *
+     */
     def "Simple test"() {
         setup:
-            Log.logPath = SysInfo.tempDir
-            Log.logFile = "test.log"
+            Log.directory = new File(SysInfo.tempDir)
+            Log.logFileName = "test.log"
             println "Log file to be created in: ${Log.logFile.absolutePath}"
         expect:
             Log.w("This is some warning")
@@ -22,7 +31,7 @@ class LogTest extends Specification {
         setup:
             Log.level = Log.Level.VERBOSE
             Log.colorInvert = true
-            Log.domain = this.class.canonicalName.tokenize('.').subList(0, 2).join('.')
+            Log.domains << this.class.canonicalName.tokenize('.').subList(0, 2).join('.')
         when:
             Log.v("This is more than you need to know... %s", "SECRET")
             Log.d("I'm %d%% that this is correct.", 80)
@@ -36,12 +45,19 @@ class LogTest extends Specification {
     static class DummyTestException extends Exception {
         String message = "This is the message of the dummy exception"
     }
+    /**
+     * In this test, only important parts of the error should be printed (Verbose is ignored)
+     */
     def "Test Exception"() {
         setup:
+            Log.level = Log.Level.ERROR
+            Log.colorInvert = true
+            Log.domains << this.class.canonicalName.tokenize('.').subList(0, 2).join('.')
             def throwIt = {
                 throw new DummyTestException()
             }
         when:
+            Log.i("This should not be printed")
             try {
                 throwIt()
             } catch(Exception e) {
@@ -77,7 +93,7 @@ class LogTest extends Specification {
             Log.color = false
             Log.w("This warning is so pale...")
         then:
-        notThrown Exception
+            notThrown Exception
     }
     def "Multiple onLog"() {
         setup:
@@ -100,8 +116,8 @@ class LogTest extends Specification {
     def "Test disable" () {
         setup:
             Log.level = Log.Level.VERBOSE
-            Log.logPath = SysInfo.tempDir
-            Log.logFile = "test.log"
+            Log.directory = new File(SysInfo.tempDir)
+            Log.logFileName = "test.log"
             if(Log.logFile.exists()) {
                 Log.logFile.delete()
             }
@@ -109,5 +125,48 @@ class LogTest extends Specification {
             Log.e("Some random error")
         expect:
             assert !Log.logFile.exists()
+    }
+    def "Test Domains in Exception"() {
+        setup:
+            Log.domains = ["invoke", "LogTest"]
+            Log.level = Log.Level.VERBOSE
+            Log.colorInvert = true
+            def throwIt = {
+                throw new DummyTestException()
+            }
+        when:
+            try {
+                throwIt()
+            } catch(Exception e) {
+                Log.e("This is an exception: ",e)
+            }
+        then:
+            /*
+             * It should highlight some lines
+             */
+            notThrown Exception
+    }
+    def "Link log"() {
+        setup:
+        Log.initialized = false //Be sure it hasn't been initialized
+        Log.level = Log.Level.VERBOSE
+        Log.directory = new File(SysInfo.tempDir)
+        Log.logFileName = "test.log"
+        if(Log.logFile.exists()) {
+            Log.logFile.delete()
+        }
+        Log.e("Some random error")
+        // We create a File object here, so we can test if it exists. It is not creating the file (it should have been created in the previous line).
+        File link = new File(Log.directory, "last-"+Log.logFileName)
+        expect:
+        assert Log.logFile.exists() : "Log file should have been created"
+        assert link.exists() : "Link was not created"
+        cleanup:
+        if(link?.exists()) {
+            link.delete()
+        }
+        if(Log.logFile.exists()) {
+            Log.logFile.delete()
+        }
     }
 }
