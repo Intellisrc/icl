d419f0c7c428ea35eb480cb08d41221f77f4d1cb
A. Lepe <dev@alepe.com>
2019-11-20 13:10:48 +0900

    2.7.2 : Added SysInfo.getFile(...)

--- modules/core/src/test/groovy/com/intellisrc/core/SysInfoTest.groovy
+++ modules/core/src/test/groovy/com/intellisrc/core/SysInfoTest.groovy
@@ -10,4 +10,28 @@ class SysInfoTest extends Specification {
         expect:
             SysInfo.getOS() != SysInfo.OS.UNKNOWN
     }
+    /**
+     * SysInfo.getFile() must generate the same path:
+     */
+    def "All paths must match - User dir"() {
+        setup:
+            File f1 = new File(new File(SysInfo.userDir, "directory"), "file.txt")
+            File f2 = SysInfo.getFile("directory", "file.txt")
+            File f3 = SysInfo.getFile(SysInfo.userDir, "directory", "file.txt")
+        expect:
+            assert f1.absolutePath == f2.absolutePath
+            assert f2.absolutePath == f3.absolutePath
+    }
+    /**
+     * SysInfo.getFile() must generate the same path:
+     */
+    def "All paths must match - Home dir"() {
+        setup:
+            File f1 = new File(new File(SysInfo.homeDir, "directory"), "file.txt")
+            File f2 = SysInfo.getFile("~/directory", "file.txt")
+            File f3 = SysInfo.getFile(SysInfo.homeDir, "directory", "file.txt")
+        expect:
+            assert f1.absolutePath == f2.absolutePath
+            assert f2.absolutePath == f3.absolutePath
+    }
 }
