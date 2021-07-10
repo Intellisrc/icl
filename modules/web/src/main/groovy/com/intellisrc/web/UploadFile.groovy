47e2aeb9a6182a3793eb195bd08160fd7df9a3e9
A. Lepe <dev@alepe.com>
2021-07-10 15:52:20 +0900

    2.8.0 Added multiple upload files in web module

---
+++ modules/web/src/main/groovy/com/intellisrc/web/UploadFile.groovy
@@ -0,0 +1,15 @@
+package com.intellisrc.web
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class UploadFile extends File {
+    final String originalName
+    final String inputName
+
+    UploadFile(String filePath, String origName, String input) {
+        super(filePath)
+        originalName = origName
+        inputName = input
+    }
+}
