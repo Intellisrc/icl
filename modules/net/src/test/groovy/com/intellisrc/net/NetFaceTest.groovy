02fa532fc65a6d2a9c3770cf28532a82c408d9f7
A. Lepe <dev@alepe.com>
2022-04-14 19:17:22 +0900

    2.8.4 Fixed UnitTests and groovy 3.0.10 compile errors

--- modules/net/src/test/groovy/com/intellisrc/net/NetFaceTest.groovy
+++ modules/net/src/test/groovy/com/intellisrc/net/NetFaceTest.groovy
@@ -9,21 +9,21 @@ import spock.lang.Specification
 class NetFaceTest extends Specification {
     def "get first ip4"() {
         setup :
-            NetFace face = LocalHost.netFaces.first()
+            NetFace face = LocalHost.localNetFace
         expect:
             assert !face.ip4List.empty
             assert face.firstIp4
     }
     def "get first ip6"() {
         setup :
-            NetFace face = LocalHost.netFaces.first()
+            NetFace face = LocalHost.localNetFace
         expect:
             assert !face.ip6List.empty
             assert face.firstIp6
     }
     def "get the ip which starts with"() {
         setup:
-            NetFace face = LocalHost.getLocalNetFace()
+            NetFace face = LocalHost.localNetFace
             Inet4Address ip = face.getIpStarts(face.firstIp4.get().hostAddress.substring(0, 5)).get()
         expect:
             assert ip.hostAddress
@@ -32,7 +32,7 @@ class NetFaceTest extends Specification {
     }
     def "get local addresses from interface"() {
         setup:
-            List<Inet4Address> addresses = LocalHost.getLocalNetFace().localAddresses
+            List<Inet4Address> addresses = LocalHost.localNetFace.localAddresses
         expect:
             assert !addresses.empty
         cleanup:
@@ -43,6 +43,6 @@ class NetFaceTest extends Specification {
     @Ignore // It may fail as it requires connection
     def "check if interface is connected"() {
         expect:
-            assert LocalHost.getLocalNetFace().isConnected()
+            assert LocalHost.localNetFace.isConnected()
     }
 }
