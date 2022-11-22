89a2ee3cc332c89d3e275d8445ac4265fc76b0e9
A.Lepe <dev@alepe.com>
2022-11-22 13:17:37 +0900

    2.8.9 Added `waitForConnection` to wait database to become online

--- modules/db/src/main/groovy/com/intellisrc/db/Database.groovy
+++ modules/db/src/main/groovy/com/intellisrc/db/Database.groovy
@@ -1,8 +1,14 @@
 package com.intellisrc.db
 
+import com.intellisrc.core.Log
+import com.intellisrc.core.Millis
+import com.intellisrc.core.SysClock
 import com.intellisrc.db.jdbc.JDBC
 import com.intellisrc.db.jdbc.JDBC.ErrorHandler
 import groovy.transform.CompileStatic
+
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 /**
  * Database initialization class
@@ -16,11 +22,8 @@ import groovy.transform.CompileStatic
 class Database {
     protected final DBPool pool
     Database(JDBC type = null, int timeout = 0, int expire = 0) {
-		if(type == null) {
-            type = JDBC.fromSettings()
-		}
         pool = new DBPool()
-        pool.init(type, timeout, expire)
+        pool.init(type ?: JDBC.fromSettings(), timeout, expire)
     }
     DB connect() {
         return new PoolConnector(pool).getDB()
@@ -30,6 +33,36 @@ class Database {
     }
     boolean isInitialized() {
         return pool?.initialized ?: false
+    }
+    /**
+     * Wait for database to be ready
+     * @param milliRetry
+     * @param milliTimeout
+     */
+    void waitForConnection(int milliRetry = Millis.SECOND, int milliTimeout = 0) {
+        boolean connected = false
+        boolean timedOut = false
+        LocalDateTime start = SysClock.now
+        Log.i("Waiting for database [%s] to become ready...", pool.jdbc.name)
+        while(!connected &&! timedOut) {
+            DB db = connect()
+            connected = db.openIfClosed()
+            if(! connected) {
+                if(milliRetry) {
+                    sleep(milliRetry)
+                }
+            }
+            db.close()
+            if(milliTimeout) {
+                timedOut = ChronoUnit.MILLIS.between(start, SysClock.now) < milliTimeout
+            }
+        }
+        if(timedOut) {
+            Log.w("Database was not ready for connections. Waited: %d ms", milliTimeout)
+        }
+        if(connected) {
+            Log.i("Connected to database: %s", pool.jdbc.name)
+        }
     }
     void onError(ErrorHandler handler) {
         pool?.jdbc?.onError = handler
