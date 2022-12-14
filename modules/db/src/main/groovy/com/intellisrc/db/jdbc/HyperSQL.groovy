f5556444c77f0275bb8e3056db9a8c65836f15c1
A.Lepe <dev@alepe.com>
2022-12-14 13:51:56 +0900

    2.8.10 [db] fixed column info for HyperSQL

--- modules/db/src/main/groovy/com/intellisrc/db/jdbc/HyperSQL.groovy
+++ modules/db/src/main/groovy/com/intellisrc/db/jdbc/HyperSQL.groovy
@@ -22,6 +22,30 @@ class HyperSQL extends JDBCServer {
     String driver = "${packageName}.JDBCDriver"
     boolean supportsBoolean = true
 
+    @Override
+    String getCatalogSearchName() {
+        return null
+    }
+
+    @Override
+    String getSchemaSearchName() {
+        return null
+    }
+
+    @Override
+    String getTableSearchName(String table) {
+        return table.toUpperCase()
+    }
+
+    @Override
+    String getTablesQuery() {
+        return "SELECT LOWER(table_name) FROM information_schema.tables WHERE table_type != 'VIEW' AND table_schema = 'PUBLIC' AND table_type = 'BASE TABLE'"
+    }
+
+    @Override
+    String getLastIdQuery(String table) {
+        return "IDENTITY()"
+    }
     // You may add more parameters as needed (values shown below are default values)
     @Override
     Map getParameters() {
