bddfd79e51f044145e1af508b169f6a5a3cfe211
A.Lepe <dev@alepe.com>
2024-04-02 12:02:10 +0900

    2.9.1 : [net] Improved Email regex to validate not so common cases, and support internationalization. Added 'strict' flag.

--- modules/net/src/main/groovy/com/intellisrc/net/Email.groovy
+++ modules/net/src/main/groovy/com/intellisrc/net/Email.groovy
@@ -3,17 +3,30 @@ package com.intellisrc.net
 import groovy.transform.CompileStatic
 
 /**
+ * This class will take a string and wrap it to ensure a
+ * valid* email is used. Be aware that the email validation
+ * here only covers a wide range of uses but not all of them,
+ * specially emails with quotation or usual domain annotations.
+ * It supports internationalization (special characters)
+ *
  * @since 10/21/17.
  */
 @CompileStatic
 class Email {
+    static String validRegex = /(?!.*(^\.|\.\.|\.@|@\.|@.*@|@-|\s|[(),:;<>\[\\\]]+))(^.+@.+\.[^@\s.]+$)/
+    static String validRegexStrict = /(?!.*(^\.|\.\.|\.@|@\.|@.*@|@-|\s|[(),:;<>\[\\\]]+))(^[\w_.+-]+@[\w_.-]+\.[^@\s.]+$)/
     @SuppressWarnings('GrFinalVariableAccess')
     final String user
     @SuppressWarnings('GrFinalVariableAccess')
     final String domain
-
-    Email(String email) throws EmailMalformedException {
-        if(isValid(email)) {
+    /**
+     * Wraps an string as an Email. It throws 'EmailMalformedException' if the email is incorrect
+     * @param email     : String containing an email
+     * @param strict    : If true, will not support emails with special characters like internationalization
+     * @throws EmailMalformedException
+     */
+    Email(String email, boolean strict = false) throws EmailMalformedException {
+        if(isValid(email, strict)) {
             def parts = email.split("@")
             user = parts[0]
             domain = parts[1]
@@ -39,7 +52,7 @@ class Email {
      * @param email
      * @return
      */
-    static boolean isValid(String email) {
-        return email.matches(/(?!.*\.\.)(^[^.@][\w_.+-]+@[\w_.+-]+\.[^@\s.]+$)/)
+    static boolean isValid(String email, boolean strict = false) {
+        return email.matches(strict ? validRegexStrict : validRegex)
     }
 }
--- modules/net/src/test/groovy/com/intellisrc/net/EmailTest.groovy
+++ modules/net/src/test/groovy/com/intellisrc/net/EmailTest.groovy
@@ -2,24 +2,61 @@ package com.intellisrc.net
 
 import spock.lang.Specification
 
-
 /**
  * @since 18/03/02.
  */
 class EmailTest extends Specification {
     def "Email Format test"() {
         expect:
-            ["me@something-this.space","aaa+333@gmail.co.jp","home@super.long.domain.with.many.subdomains.com.mx"].each {
-                assert new Email(it)
-            }
-        when:
-            new Email("me@som\\ething-this.space")
-        then:
-            thrown Email.EmailMalformedException
-        when:
-            new Email("aaa@333@gmail.co.jp")
-        then:
-            thrown Email.EmailMalformedException
+            assert Email.isValid(email, strict) == valid
+        where:
+            // (some from: https://gist.github.com/cjaoude/fd9910626629b53c4d25)
+            email                                                   | valid     | strict
+            // Valid cases
+            "me@something-this.space"                               | true      | false
+            "aaa+333@gmail.co.jp"                                   | true		| false
+            "home@super.long.domain.with.many.subdomains.com.mx"    | true		| false
+            "a@a.jp"                                                | true		| false
+            "firstname.lastname@example.com"                        | true		| false
+            "email@subdomain.example.com"                           | true		| false
+            "firstname+lastname@example.com"                        | true		| false
+            "email@123.123.123.123"                                 | true		| false
+            "1234567890@example.com"                                | true		| false
+            "email@example-one.com"                                 | true		| false
+            "_______@example.com"                                   | true		| false
+            "email@example.name"                                    | true		| false
+            "email@example.museum"                                  | true		| false
+            "firstname-lastname@example.com"                        | true		| false
 
+            // Strict cases
+            "áÇÈñÏç@example.com"                                    | true		| false
+            "honto@本当.jp"                                          | true		| false
+            "あいうえお@example.com"                                  | true      | false
+            "áÇÈñÏç@example.com"                                    | false 	| true
+            "honto@本当.jp"                                          | false		| true
+            "あいうえお@example.com"                                  | false     | true
+
+            // Invalid cases
+            "me@som\\ething-this.space"                             | false		| false
+            "aaa@333@gmail.co.jp"                                   | false		| false
+            ".aaa@gmail.co.jp"                                      | false		| false
+            "plainaddress"                                          | false		| false
+            '#@%^%#$@#$@#.com'                                      | false		| false
+            "@example.com"                                          | false		| false
+            "Joe Smith <email@example.com>"                         | false		| false
+            "email.example.com"                                     | false		| false
+            "email@example@example.com"                             | false		| false
+            ".email@example.com"                                    | false		| false
+            "email.@example.com"                                    | false		| false
+            "email..email@example.com"                              | false		| false
+            "email@example.com (Joe Smith)"                         | false		| false
+            "email@example"                                         | false		| false
+            "email@-example.com"                                    | false		| false
+            //"email@111.222.333.44444"                               | false		| false
+            "email@example..com"                                    | false		| false
+            "Abc..123@example.com"                                  | false		| false
+            '(),:;<>[\\]@example.com'                               | false		| false
+            //'just"not"right@example.com'                            | false		| false
+            'this\\ is"really"not\\allowed@example.com'             | false		| false
     }
 }
