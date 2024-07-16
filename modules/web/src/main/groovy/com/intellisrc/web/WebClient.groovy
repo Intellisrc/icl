7e6a98e02f7ee2eea919b43d57274db439dea4b6
A.Lepe <dev@alepe.com>
2024-07-16 11:38:47 +0900

    2.9.2 : [web] WebClient: added "asBody" param for GET, headers and timeout.

--- modules/web/src/main/groovy/com/intellisrc/web/WebClient.groovy
+++ modules/web/src/main/groovy/com/intellisrc/web/WebClient.groovy
@@ -1,13 +1,20 @@
 package com.intellisrc.web
 
+import com.intellisrc.core.Millis
 import com.intellisrc.etc.JSON
 import com.intellisrc.etc.Mime
 import groovy.transform.CompileStatic
 import org.eclipse.jetty.http.HttpMethod
 
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+
 /**
  * This class is a wrapper around HttpURLConnection
  * This client is designed to send a body as string (either encoded in JSON or not)
+ * or specify custom request headers
  * and process the return per line or all at once
  * For simple GET requests, use `myUrlWithParams.toURL().text` (Groovy)
  * @since 2024/05/24.
@@ -16,7 +23,9 @@ import org.eclipse.jetty.http.HttpMethod
 class WebClient {
     final URL url
     String charset = "UTF-8"
+    int timeout = Millis.SECOND_10
     Output eachLine = null
+    protected boolean getAsBody = true
 
     static interface Output {
         void call(String out)
@@ -35,69 +44,105 @@ class WebClient {
      * Call url with a query
      * @param query
      */
-    void get(Map query, Output onResponse = null) {
-        request(query, onResponse, HttpMethod.GET)
+    void get(Map query, Output onResponse = null, boolean asBody = true, Map<String, String> headers = [:]) {
+        this.getAsBody = asBody
+        request(query, onResponse, HttpMethod.GET, headers)
     }
     /**
      * POST using plain text body
      * @param data
      */
-    void post(String data, Output onResponse = null) {
-        request(data, onResponse, HttpMethod.POST)
+    void post(String data, Output onResponse = null, Map<String, String> headers = [:]) {
+        request(data, onResponse, HttpMethod.POST, headers)
     }
     /**
      * POST using JSON data
      * @param data
      */
-    void post(Map data, Output onResponse = null) {
-        request(data, onResponse, HttpMethod.POST)
+    void post(Map data, Output onResponse = null, Map<String, String> headers = [:]) {
+        request(data, onResponse, HttpMethod.POST, headers)
     }
 
     /**
-     * Performa the request
+     * Perform the request
      * @param data
      * @param onResponse
      * @param method
      */
-    void request(Object data, Output onResponse = null, HttpMethod method) {
-        HttpURLConnection con = (HttpURLConnection) url.openConnection()
-        con.doOutput = true
-        String body
-        //noinspection GroovyFallthrough
-        switch (data) {
-            case Map :
-            case List :
-                con.setRequestProperty("Accept", Mime.JSON)
-                body = JSON.encode(data)
-                break
-            default:
-                con.requestMethod = method.toString()
-                body = data.toString()
-                break
-        }
-        try(OutputStream os = con.getOutputStream()) {
-            byte[] input = body.getBytes(charset)
-            os.write(input, 0, input.length)
-        }
-        try(BufferedReader br = new BufferedReader(
-        new InputStreamReader(con.inputStream, charset))) {
-            StringBuilder response = new StringBuilder()
-            String responseLine
-            while ((responseLine = br.readLine()) != null) {
-                if(eachLine) {
-                    eachLine.call(responseLine.trim())
+    void request(Object data, Output onResponse = null, HttpMethod method, Map<String, String> headers = [:]) {
+        if(method == HttpMethod.GET &&! getAsBody) {
+            HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(url.toURI())
+                .timeout(Duration.ofMillis(timeout))
+                .method(method.toString(), HttpRequest.BodyPublishers.noBody())
+            if(! headers.isEmpty()) {
+                headers.each {
+                    builder.header(it.key, it.value)
                 }
-                response.append(responseLine.trim())
             }
-            if(onResponse) {
+            HttpRequest request = builder.build()
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString())
+            if (onResponse) {
                 switch (onResponse) {
                     case JsonOutput:
-                        (onResponse as JsonOutput).call(JSON.decode(response.toString()) as Map)
+                        (onResponse as JsonOutput).call(JSON.decode(response.body()) as Map)
                         break
                     default:
                         onResponse.call(response.toString())
                         break
                 }
+            }
+        } else {
+            HttpURLConnection con = (HttpURLConnection) url.openConnection()
+            if (onResponse) {
+                con.doOutput = true
+            }
+            con.requestMethod = method.toString()
+            con.readTimeout = timeout
+            con.connectTimeout = timeout
+            String body
+            //noinspection GroovyFallthrough
+            switch (data) {
+                case Map:
+                case List:
+                    con.setRequestProperty("Accept", Mime.JSON)
+                    body = JSON.encode(data)
+                    break
+                default:
+                    body = data.toString()
+                    break
+            }
+            if (!headers.isEmpty()) {
+                headers.each {
+                    con.setRequestProperty(it.key, it.value)
+                }
+            }
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = body.getBytes(charset)
+                os.write(input, 0, input.length)
+            }
+            try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.inputStream, charset))) {
+                StringBuilder response = new StringBuilder()
+                String responseLine
+                while ((responseLine = br.readLine()) != null) {
+                    if (eachLine) {
+                        eachLine.call(responseLine.trim())
+                    }
+                    response.append(responseLine.trim())
+                }
+                br.close()
+                if (onResponse) {
+                    switch (onResponse) {
+                        case JsonOutput:
+                            (onResponse as JsonOutput).call(JSON.decode(response.toString()) as Map)
+                            break
+                        default:
+                            onResponse.call(response.toString())
+                            break
+                    }
+                }
+                con.disconnect()
             }
         }
     }
