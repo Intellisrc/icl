03edd325aeefc1db1fdc1dd73886034d731e792b
A.Lepe <dev@alepe.com>
2026-01-20 17:05:45 +0900

    2.10.0 : Improved Redis tests

--- modules/etc/src/test/groovy/com/intellisrc/etc/RedisManualTest.groovy
+++ modules/etc/src/test/groovy/com/intellisrc/etc/RedisManualTest.groovy
@@ -18,6 +18,29 @@ class RedisManualTest {
         redis2.delete("hello")
         assert ! redis2.exists("hello")
 
+        Redis redis3 = new Redis("adv", ":")
+        redis3.set("list", [1,2,3,4,5,6])
+
+        redis.clear() // Previous prefix (should not affect "adv" prefix
+        assert redis3.get("list",[]).collect { it as int }.contains(3)
+
+        // will be converted to hset so values become string (numeric)
+        // this is done by redis, so no much we can do
+        redis3.set("map",[ok: false])
+        assert redis3.get("map",[:]).ok == "0"
+
+        redis3.set("url","http://localhost".toURL())
+        assert redis3.get("url").toURL().host == "localhost"
+
+        redis3.set("num", 30000)
+        assert redis3.get("num", 0) == 30000
+
+        redis3.set("dbl", 100.4d)
+        assert redis3.get("dbl", 0d) == 100.4d
+
+        redis3.clear()
+        assert redis3.keys.empty
+
         println "✅ Test finished correctly"
         Redis.quit()
     }
