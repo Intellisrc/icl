50e260c27a99b9de873c9311612ff005543e0712
Your Name <dev@alepe.com>
2026-06-08 11:09:42 +0900

    2.9.7 [log] Fixed initialization issue in CommonLogger and added keywords to ignore list

--- modules/core/src/main/groovy/com/intellisrc/core/props/StringPropertiesSet.groovy
+++ modules/core/src/main/groovy/com/intellisrc/core/props/StringPropertiesSet.groovy
@@ -4,7 +4,9 @@ import com.intellisrc.core.Log
 import groovy.transform.CompileStatic
 
 import java.lang.reflect.Field
-import java.time.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 /**
  * This class is a partial implementation
